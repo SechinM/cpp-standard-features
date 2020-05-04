cae2f197eb2ec35142d10331ade2f24933493bb1
m.sechin <m.sechin@avantize.com>
2020-05-04 13:52:01 +0300

    Added std::filesystem example.

--- cpp17/std::filesystem/main.cpp
+++ cpp17/std::filesystem/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <filesystem>
 
@@ -5,8 +6,24 @@
 // std::filesystem
 //
 
-int main()
+namespace detail
 {
 
+static const std::string FileName{ "test.txt" };
+static const std::string NewDirName{ "test_me" };
+static const std::string Tmp{ "/tmp" };
 
+}
+
+int main()
+{
+	auto path{ std::filesystem::path(detail::Tmp) };
+	auto pathToFile{ path };
+	std::ofstream(pathToFile.append(detail::FileName)).put('a');
+	std::filesystem::create_directory(path.append(detail::NewDirName));
+
+	if (std::filesystem::exists(path) && std::filesystem::exists(pathToFile))
+	{
+		std::filesystem::copy_file(pathToFile, path.append(detail::FileName));
+	}
 }
